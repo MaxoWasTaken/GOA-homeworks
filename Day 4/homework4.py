f8684c435cb8c6cc898f7beca20181889e8c9107
MaxoWasTaken <maxowastaken@gmail.com>
2024-03-10 11:29:19 +0400

    second commit

---
+++ Day 4/homework4.py
@@ -0,0 +1,6 @@
+name = input("what is your name?: ")
+age = int(input("How old are you?: "))
+future = int(input("how much you wanna travel into future?: "))
+
+future_age = age + future
+print("You will be "+str(future_age)+" years old in that time")
