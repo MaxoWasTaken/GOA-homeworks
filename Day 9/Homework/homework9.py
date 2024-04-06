dad0883eec84bf8d806c092691928bdcaae05539
MaxoWasTaken <maxowastaken@gmail.com>
2024-04-06 22:46:29 +0400

    second commit

---
+++ Day 9/Homework/homework9.py
@@ -0,0 +1,10 @@
+income = int(input("What is your income?: "))
+
+if income > 10000:
+    print("did you learn in goa?")
+
+elif income > 1000:
+    print("youre mid")
+
+else:
+    print("you shouldve learned in goa")
---
+++ Day 9/classwork/ok.py
@@ -0,0 +1,14 @@
+# num1 = int(input("please eter first number: "))
+# num2 = int(input("please enter second number: "))
+
+# if num1 > num2: 
+#    print("GOA")
+
+
+# mom = "idk"
+# dad = "ora"
+# brother = "qew"
+# sister = "bleh"
+
+# if mom == "idk" and dad == "ora" or brother == "qew" and sister == "bleh": 
+#    print("True")
