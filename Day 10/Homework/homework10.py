84d240c3d617480bf2e2a020b6d6061335857062
MaxoWasTaken <maxowastaken@gmail.com>
2024-04-13 17:37:27 +0400

    second commit

---
+++ Day 10/Homework/homework10.py
@@ -0,0 +1,69 @@
+# homework 1
+
+# question = input("Which academy do you learn coding in?: ")
+
+# if question == ("GOA"):
+#    print("true")
+
+# else:
+#    print("false")
+
+# homework 2
+
+# item_price = int(input("What is price of item you wanna buy?: "))
+
+# budget = int(input("What is your budget?: "))
+
+# if budget >= item_price:
+#    print("you have enough money")
+#
+# else:
+#    print(item_price - budget)
+
+#  homework 3
+
+# number = int(input("type a number: "))
+# 
+# if number >= 5:
+#    print(number * 2)
+#
+# else:
+#    print(number * 4)
+
+# homework 4
+
+# tickets = int(input("How many tickets you wanna buy?: "))
+
+# price = 10
+
+# discount_price = 7
+
+# if tickets > 5:
+#    print(tickets * discount_price)
+#
+#
+# else:
+#    print(tickets * price)
+
+# homework 5
+
+# num1 = int(input("Type number 1: "))
+
+# num2 = int(input("Type number 2: "))
+
+# question = input("Do you want to use +,-, / or * in equation?: ")
+
+# if question == ("+"):
+#    print(num1 + num2)
+#
+# elif question == ("-"):
+#    print(num1 - num2)
+#
+# elif question == ("/"):
+#    print(num1 / num2)
+#
+# elif question == ("*"):
+#    print(num1 * num2)
+#
+# else:
+#    print("pick either +,-, / or * to calculate")
---
+++ Day 10/classwork/classwork10.py
@@ -0,0 +1,7 @@
+age = int(input("How old are you?: "))
+
+if age > 17:
+    print("your an adult")
+
+else:
+    print("youre a kid")
