29a1592985477e86d9547a53e50810f4e79b915f
MaxoWasTaken <maxowastaken@gmail.com>
2024-04-27 16:37:21 +0400

    second commit

---
+++ Day 12/Classwork/cw.py
@@ -0,0 +1,6 @@
+# for i in range(10): 
+#  print("maxo")
+
+for i in range(4):
+  num = num + 2
+  print(num)
---
+++ Day 12/Homework/Homework.py
@@ -0,0 +1,28 @@
+# Homework 1
+
+# for x in range(1, 10, 2):
+#  print(x)
+
+
+# Homework 2
+
+# for x in range(5, 55, 5):
+#  print(x)
+
+
+# Homework 3
+
+#num = int(input("enter a number: "))
+
+# for x in range(0, num):
+#  print(x)
+
+
+# Homework #4
+
+# total_sum = 0
+# for num in range(51): 
+#    print(num)  
+#    total_sum += num
+
+# print("The sum of numbers from 0 to 50 is:", total_sum)
