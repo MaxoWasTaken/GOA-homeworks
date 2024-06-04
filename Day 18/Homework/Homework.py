83dac2dd4470228367fed9cabe1326ec902d602b
MaxoWasTaken <maxowastaken@gmail.com>
2024-06-05 00:39:08 +0400

    second commit

---
+++ Day 18/Homework/Homework.py
@@ -0,0 +1,15 @@
+# 1st method
+elements = ['$', '%', '^', 'V']
+
+middle_elements_normal = elements[1:3]
+
+print(middle_elements_normal)
+
+
+
+# 2nd method
+elements = ['$', '%', '^', 'V']
+
+middle_elements_negative = elements[-3:-1]
+
+print(middle_elements_negative)
